d1748218a3b8036d4921c6cc6815e4a518b83dc9
Hansi0711 <learninggurl11@gmail.com>
2014-09-02 17:00:45 +0530

    Update FizzBuzz.java

--- codeeval/easy/FizzBuzz.java
+++ codeeval/easy/FizzBuzz.java
@@ -1,21 +1,13 @@
-public class FizzBuzz {                      // Everything in Java is a class
-	  public static void main(String[] args) {
-		 
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter the first number");
-		int firstNum = scan.nextInt();
-		System.out.println("Enter the second number");
-		int secondNum = scan.nextInt();
-		System.out.println("Enter the length");
-		int numLength = scan.nextInt();
-	    for(int i = 1; i <= numLength; i++) {                    // count from 1 to given length
-	      if (((i % firstNum) == 0) && ((i % secondNum) == 0))            // A multiple of both?
-	        System.out.print("fizzbuzz");    
-	      else if ((i % firstNum) == 0) System.out.print("fizz"); // else a multiple of first number?
-	      else if ((i % secondNum) == 0) System.out.print("buzz"); // else a multiple of second number?
-	      else System.out.print(i);                        // else just print it
-	      System.out.print(" "); 
-	    }
-	    System.out.println();
-	  }
-	}
+//This code is w.r.t codeval site.
+if (lineArray.length > 0) {
+    for(int i = 1; i <= Integer.valueOf(lineArray[2]); i++) {                    
+	if (((i % Integer.valueOf(lineArray[0])) == 0) && ((i % Integer.valueOf(lineArray[1])) == 0))
+	System.out.print("FB");    
+	else if ((i % Integer.valueOf(lineArray[0])) == 0) System.out.print("F"); 
+	else if ((i % Integer.valueOf(lineArray[1])) == 0) System.out.print("B");
+	else System.out.print(i);                        
+	System.out.print(" "); 
+    }
+	
+}
+      System.out.println();
